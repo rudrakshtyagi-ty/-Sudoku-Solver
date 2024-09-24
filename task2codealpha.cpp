081df8028f3e5041cadea6ec9878b83d8b4752fa
rudrakshtyagi-ty <Rudrakshtyagi619@gmail.com>
2024-09-24 12:31:08 +0530

    Add files via upload

---
+++ task2codealpha.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#define N 9
+
+// Function to print the Sudoku grid
+void printGrid(int grid[N][N]) {
+    for (int row = 0; row < N; row++) {
+        for (int col = 0; col < N; col++) {
+            std::cout << grid[row][col] << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
+// Function to check if placing num at grid[row][col] is valid
+bool isValid(int grid[N][N], int row, int col, int num) {
+    for (int x = 0; x < N; x++) {
+        if (grid[row][x] == num) {
+            return false;
+        }
+    }
+    for (int x = 0; x < N; x++) {
+        if (grid[x][col] == num) {
+            return false;
+        }
+    }
+
+    int startRow = row - row % 3, startCol = col - col % 3;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (grid[i + startRow][j + startCol] == num) {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+bool findEmptyCell(int grid[N][N], int &row, int &col) {
+    for (row = 0; row < N; row++) {
+        for (col = 0; col < N; col++) {
+            if (grid[row][col] == 0) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+bool solveSudoku(int grid[N][N]) {
+    int row, col;
+    if (!findEmptyCell(grid, row, col)) {
+        return true;
+    }
+
+    for (int num = 1; num <= 9; num++) {
+        if (isValid(grid, row, col, num)) {
+            grid[row][col] = num;
+
+            if (solveSudoku(grid)) {
+                return true;
+            }
+
+            grid[row][col] = 0;
+        }
+    }
+
+    return false;
+}
+
+int main() {
+    int grid[N][N] = {
+        {5, 3, 0, 0, 7, 0, 0, 0, 0},
+        {6, 0, 0, 1, 9, 5, 0, 0, 0},
+        {0, 9, 8, 0, 0, 0, 0, 6, 0},
+        {8, 0, 0, 0, 6, 0, 0, 0, 3},
+        {4, 0, 0, 8, 0, 3, 0, 0, 1},
+        {7, 0, 0, 0, 2, 0, 0, 0, 6},
+        {0, 6, 0, 0, 0, 0, 2, 8, 0},
+        {0, 0, 0, 4, 1, 9, 0, 0, 5},
+        {0, 0, 0, 0, 8, 0, 0, 7, 9}
+    };
+
+    if (solveSudoku(grid)) {
+        std::cout << "Sudoku solved successfully:\n";
+        printGrid(grid);
+    } else {
+        std::cout << "No solution exists.";
+    }
+
+    return 0;
+}
